e26bea47852714a9f6399cfb014b346fc8473f71
icedsquaredr <yeokh@i2r.a-star.edu.sg>
2015-05-12 15:32:40 +0800

    Minor cosmetic edits.  - reduced blank lines  - separated comment symbols.  - Also fixed minor typos.

--- data_mining/svm/spam example.R
+++ data_mining/svm/spam example.R
@@ -8,14 +8,11 @@ library(kernlab)
 
 
 
-# load the inbuil dataset
+# load the inbuilt dataset
 data(spam)
 
-#see spam in the table
+# see spam in the table
 View(spam)
-
-
-
 
 ################################################################################
 # divide the spam data into the training set and testing set
@@ -31,21 +28,19 @@ View(spamtrain)
 spamtest <- spam[index[((ceiling(dim(spam)[1]/2)) + 1):dim(spam)[1]], ]
 View(spamtest)
 
-
 ###########################################################
 # training SVM
 
-#function instruction 
+# function instruction
 ?ksvm
 
 # system.time(filter <- ksvm(type~.,data=spamtrain,kernel="rbfdot",kpar=list(sigma=0.05),C=5,cross=3))
 filter <- ksvm(type~.,data=spamtrain,kernel="rbfdot",kpar=list(sigma=0.05),C=5,cross=3,prob.model=T)
 filter
 
-
 ## result interpretation
 
-#number of SV
+# number of SV
 filter@nSV
 
 # row numbers of boundary point (support vector)
@@ -58,9 +53,6 @@ View(spam[index[unlist(filter@alphaindex)], ])
 #importance of the boundary points
 unlist(filter@alpha)
 
-
-
-
 #########################################################################
 ## predict mail type on the test set
 mailtype <- predict(filter,spamtest[,-58])
@@ -68,17 +60,7 @@ head(mailtype)
 
 # prediction result
 View(cbind(truth = spamtest[,58],prediction = mailtype))
+
 ## Check results
 table(mailtype,spamtest[,58])
-
-
-
-
-
-
-
-
-
-
-
 
